94f55d42fc92c8305a741ee72fc95f0e6aedd372
Suraj <122242670+Kiran9991@users.noreply.github.com>
2023-12-09 19:00:41 +0530

    Added css to navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import Products from "./components/Product/Products";
@@ -20,30 +20,26 @@ function App() {
     setShowCart(true);
   };
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <CartProvider>
-          <ModalCart show={showCart} onHide={onClose} />
-          <Header onClick={show} />
-        </CartProvider>
-      ),
-      children: [
-        {
-          path: "/products",
-          element: (
-            <Products/>
-          ),
-        },
-        { path: "/about", element: <AboutUs /> },
-        { path: '/', element: <Home/> },
-        { path: '/contact-us', element: <ContactUs/>}
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+    <CartProvider>
+      <ModalCart show={showCart} onHide={onClose} />
+      <Header onClick={show} />
+      <main>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+        <Route path="/about">
+          <AboutUs />
+        </Route>
+        <Route path="/contact-us">
+          <ContactUs />
+        </Route>
+      </main>
+    </CartProvider>
+  );
 }
 
 export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,56 +1,32 @@
 import React from "react";
-import { Outlet, Link, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import { Navbar, Container, Nav } from "react-bootstrap";
 import HeaderCartButton from "./HeaderCartButton";
 import header from "./Header.module.css";
 
 const Header = (props) => {
-  const location = useLocation();
   return (
-    <>
-      <Navbar expand="sm" bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand>E - Commerce</Navbar.Brand>
-          <Nav className="me-auto">
-            <Link
-              to="/"
-              className={`${header.navlink} ${
-                location.pathname === "/" ? header.active : ""
-              }`}
-            >
-              Home
-            </Link>
-            <Link
-              to="/products"
-              className={`${header.navlink} ${
-                location.pathname === "/products" ? header.active : ""
-              }`}
-            >
-              Store
-            </Link>
-            <Link
-              to="/about"
-              className={`${header.navlink} ${
-                location.pathname === "/about" ? header.active : ""
-              }`}
-            >
-              About
-            </Link>
-            <Link
-              to="/contact-us"
-              className={`${header.navlink} ${
-                location.pathname === "/contact-us" ? header.active : ""
-              }`}
-            >
-              Contact Us
-            </Link>
-          </Nav>
-          <HeaderCartButton onClick={props.onClick} />
-        </Container>
-      </Navbar>
-      <Outlet />
-    </>
+    <Navbar expand="sm" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand>E - Commerce</Navbar.Brand>
+        <Nav className="me-auto">
+          <NavLink activeClassName={header.navLinks} className={header.navInactive} to="/home">
+            Home
+          </NavLink>
+          <NavLink activeClassName={header.navLinks} className={header.navInactive} to="/products">
+            Store
+          </NavLink>
+          <NavLink activeClassName={header.navLinks} className={header.navInactive} to="/about">
+            About
+          </NavLink>
+          <NavLink activeClassName={header.navLinks} className={header.navInactive} to="/contact-us">
+            Contact Us
+          </NavLink>
+        </Nav>
+        <HeaderCartButton onClick={props.onClick} />
+      </Container>
+    </Navbar>
   );
 };
 
